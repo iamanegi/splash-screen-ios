89764d43077dae6f45a244e6477870c219a8bf4c
aman <amannegi229@gmail.com>
2022-09-10 12:25:49 +0530

    Initial Commit

---
+++ splashscreen/splashscreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  splashscreenApp.swift
+//  splashscreen
+//
+//  Created by Aman Negi on 10/09/22.
+//
+
+import SwiftUI
+
+@main
+struct splashscreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
